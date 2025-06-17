6f78bd035992707fa389555d663499884bd3f224
Rei_Gin <marhamdiihwal70@gmail.com>
2025-06-17 14:53:52 +0700

    22

--- myblog/echo/models.py
+++ myblog/echo/models.py
@@ -11,12 +11,10 @@ class Sonata(models.Model):
         verbose_name = "Sonata"
         verbose_name_plural = "Sonatas"
 
-# Model untuk Echo (bidang 'skill' dihapus)
 class Echo(models.Model):
     name = models.CharField(max_length=100, unique=True)
     cost = models.IntegerField()
     sonatas = models.ManyToManyField(Sonata, related_name='echos')
-    # Bidang 'skill' telah dihapus dari sini
 
     def __str__(self):
         return self.name
